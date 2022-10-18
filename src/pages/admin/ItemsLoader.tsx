07631dfb1c32fcdd084b4f79fc9aa522782922be
kirtarget <sheldongriffiths1@gmail.com>
2022-10-18 13:49:29 +0300

    fixed more bugs, edited navbar

--- src/pages/admin/ItemsLoader.tsx
+++ src/pages/admin/ItemsLoader.tsx
@@ -1,18 +1,19 @@
-import React, { useState } from "react"
+import React, { useEffect, useState } from "react"
 import { trpc } from "../../utils/trpc"
 import { Item } from "../../lib/types/apiTypes"
 import { useBearStore } from "../../store/store"
+import { useHasMounted } from "../../Hooks/hasMounted"
 
 export default function ItemsLoader(): JSX.Element {
   const [isEditing, setIsEditing] = useState<boolean | null>(null)
 
-  const { data } = trpc.items.useQuery({ id: "client" })
+  const hasMounted = useHasMounted()
 
-  if (!data) {
-    return <div>Loading...</div>
-  }
+  const { data } = trpc.items.useQuery({ id: "all" })
 
-  useBearStore.setState({ items: data.items })
+  useEffect(() => {
+    useBearStore.setState({ items: data?.items })
+  }, [data])
 
   let items = useBearStore((state) => state.items)
 
@@ -23,35 +24,39 @@ export default function ItemsLoader(): JSX.Element {
     // mutation.mutate({})
   }
 
+  if (!data) {
+    return <div>Loading...</div>
+  }
   return (
     <div className="px-auto">
-      {items.map((item: Item) => {
-        if (!item.mainImage.startsWith("http")) {
-          item.mainImage = "https://trpc.io/img/logo.svg"
-        }
-        return (
-          <div key={item.id} className="flex my-1 p-2">
-            <a
-              href="#"
-              className="flex flex-col items-center bg-white rounded-lg border shadow-md md:flex-row md:max-w-xl hover:bg-gray-100 dark:border-gray-700 dark:bg-gray-800 dark:hover:bg-gray-700 w-full"
-            >
-              <img
-                className="object-cover w-full h-96 rounded-t-lg md:h-auto md:w-48 md:rounded-none md:rounded-l-lg"
-                src={item.mainImage}
-                alt=""
-              />
-              <div className="flex flex-col justify-between p-4 leading-normal">
-                <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
-                  {item.name}
-                </h5>
-                <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">
-                  {item.description}
-                </p>
-              </div>
-            </a>
-          </div>
-        )
-      })}
+      {hasMounted &&
+        items.map((item: Item) => {
+          if (!item.mainImage.startsWith("http")) {
+            item.mainImage = "https://trpc.io/img/logo.svg"
+          }
+          return (
+            <div key={item.id} className="flex my-1 p-2">
+              <a
+                href="#"
+                className="flex flex-col items-center bg-white rounded-lg border shadow-md md:flex-row md:max-w-xl hover:bg-gray-100 dark:border-gray-700 dark:bg-gray-800 dark:hover:bg-gray-700 w-full"
+              >
+                <img
+                  className="object-cover w-full h-96 rounded-t-lg md:h-auto md:w-48 md:rounded-none md:rounded-l-lg"
+                  src={item.mainImage}
+                  alt=""
+                />
+                <div className="flex flex-col justify-between p-4 leading-normal">
+                  <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
+                    {item.name}
+                  </h5>
+                  <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">
+                    {item.description}
+                  </p>
+                </div>
+              </a>
+            </div>
+          )
+        })}
     </div>
   )
 }
