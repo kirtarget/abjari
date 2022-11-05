a1c44c5696e5e4ed22cbd23e155ee3056c5dc8b8
kirtarget <sheldongriffiths1@gmail.com>
2022-11-05 21:09:39 +0300

    removed git cache

--- prisma/migrations/20221020194950_added_sizes/migration.sql
+++
@@ -1,8 +0,0 @@
--- CreateEnum
-CREATE TYPE "Size" AS ENUM ('S', 'M', 'L', 'XL');
-
--- AlterTable
-ALTER TABLE "Item" ADD COLUMN     "size" "Size"[] DEFAULT ARRAY['S', 'XL', 'M', 'L']::"Size"[];
-
--- AlterTable
-ALTER TABLE "User" ADD COLUMN     "Role" "Role" NOT NULL DEFAULT 'USER';
--- src/components/MainPage/WearUs.tsx
+++
@@ -1,7 +0,0 @@
-import { Container } from "@mui/material";
-
-const WearUs = () => {
-  return <Container>They wear us</Container>;
-};
-
-export default WearUs;
